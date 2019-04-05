0b1e9a8769c86851b0fd2296e9dc35149dccd61e
Amit <29959138+maverickamit@users.noreply.github.com>
2019-04-05 16:59:01 +0530

    Added function names
    Added function names "buildQuiz" and "showResults".  And added button event listener to show results onclick.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import "./styles.css";
 
-document.getElementById("quiz").innerHTML = `
-
+let quizContainer = document.getElementById("quiz");
+let resultsContainer = document.getElementById("results");
+let submitButton = document.getElementById("button");
+quizContainer.innerHTML = `
 `;
+
+function buildQuiz() {}
+
+function showResults() {}
+
+//Displays Quiz
+buildQuiz();
+//On submit, shows Results
+submitButton.addEventListener("click", showResults);
