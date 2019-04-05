a06c600b18fc4bec00ab2901473c716407e2574a
Amit <29959138+maverickamit@users.noreply.github.com>
2019-04-05 20:22:58 +0530

    Added Questions array with answers

--- src/index.js
+++ src/index.js
@@ -14,3 +14,26 @@ function showResults() {}
 buildQuiz();
 //On submit, shows Results
 submitButton.addEventListener("click", showResults);
+
+let myQuestions = [
+  {
+    question:
+      "Zeno and Chrysippus believed that many of the sins were equal. Who was the German philosopher who also shared this similar thinking?",
+    answers: {
+      1: "Nietzsche",
+      2: "Kant",
+      3: "Hegel",
+      4: "Herder"
+    },
+    correctAnswer: "2"
+  },
+  {
+    question: "Who is the strongest?",
+    answers: {
+      a: "Superman",
+      b: "The Terminator",
+      c: "Waluigi, obviously"
+    },
+    correctAnswer: "c"
+  }
+];
