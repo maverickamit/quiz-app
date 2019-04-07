617ad5fd0366083ce0eb904eb417a50ec67cde45
Amit <29959138+maverickamit@users.noreply.github.com>
2019-04-07 20:21:46 +0530

    added showResults function
    showResults function evaluates submitted answers and displays score.

--- src/index.js
+++ src/index.js
@@ -2,44 +2,7 @@ import "./styles.css";
 
 let quizContainer = document.getElementById("quiz");
 let resultsContainer = document.getElementById("results");
-let submitButton = document.getElementById("button");
-quizContainer.innerHTML = `
-`;
-
-function buildQuiz(){
-  // to store html output
-  let output=[];
-
-  myQuestions.forEach((currentQuestion, questionNumber) =>{
-
-let answers=[];
-for (options in currentQuestion.answers)
-{
-  answers.push(
-    `<label>
-    <input type="radio" name="question${questionNumber} value="${options}">
-    ${options} : ${currentQuestion.answers[options]}
-    </label> `);
-}
-// add this question and it's answers to the output
-output.push(
-  `<div class="questions">${currentQuestion.question}</div>
-  <div class="answers">${answers.join("")}</div>
-  `
-);
-}
-);
-//combine output list into one string of html 
-
-quizContainer.innerHTML= output.join("");
-
-function showResults() {}
-
-//Displays Quiz
-buildQuiz();
-//On submit, shows Results
-submitButton.addEventListener("click", showResults);
-
+let submitButton = document.getElementById("submit");
 let myQuestions = [
   {
     question:
@@ -61,3 +24,64 @@ let myQuestions = [
     correctAnswer: "c"
   }
 ];
+
+function buildQuiz() {
+  // to store html output
+  let output = [];
+
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    let options;
+    let answers = [];
+    for (options in currentQuestion.answers) {
+      answers.push(
+        `<label>
+    <input type="radio" name="question${questionNumber}" value="${options}">
+    ${options} : ${currentQuestion.answers[options]}
+    </label> `
+      );
+    }
+    // add this question and it's answers to the output
+    output.push(
+      `<div class="questions">${currentQuestion.question}</div>
+  <div class="answers">${answers.join("")}</div>
+  `
+    );
+  });
+  //combine output list into one string of html
+
+  quizContainer.innerHTML = output.join("");
+}
+
+function showResults() {
+  //gather all answers in a container
+  let answerContainers = quizContainer.querySelectorAll(".answers");
+
+  //keeping track of user's answers
+  let numCorrect = 0;
+
+  // for each question
+
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    //find selected answer
+    let answerContainer = answerContainers[questionNumber];
+    let selector = `input[name=question${questionNumber}]:checked`;
+    let userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if (userAnswer === currentQuestion.correctAnswer) {
+      //add score
+      numCorrect++;
+      //green color
+      answerContainer.style.color = "lightgreen";
+    } else {
+      answerContainer.style.color = "red";
+    }
+    //shows number of correct reuslts out of total
+    resultsContainer.innerHTML = numCorrect + " out of " + myQuestions.length;
+  });
+}
+
+//Displays Quiz
+buildQuiz();
+//On submit, shows Results
+submitButton.addEventListener("click", showResults);
